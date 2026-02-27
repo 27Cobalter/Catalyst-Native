cdf61e92532e37be201543cc533cb5376e2dd382
MOCHIZUKI Natsune <me@natsuneko.cat>
2026-02-27 21:18:19 +0900

    feat: add Sentry SDK

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,6 +10,23 @@ import "react-native-reanimated";
 import "../globals.css";
 
 import { useColorScheme } from "@/hooks/use-color-scheme";
+import * as Sentry from "@sentry/react-native";
+import { useEffect, useState } from "react";
+
+Sentry.init({
+  dsn: "https://6d7c270e3a7bb56c0a746319d7e885d5@o4504564074348544.ingest.us.sentry.io/4510957726793728",
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Enable Logs
+  enableLogs: true,
+  integrations: [Sentry.feedbackIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
 
 SplashScreen.preventAutoHideAsync();
 
@@ -17,8 +34,23 @@ export const unstable_settings = {
   anchor: "(tabs)",
 };
 
-export default function RootLayout() {
+export default Sentry.wrap(function RootLayout() {
   const colorScheme = useColorScheme();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      // setIsLoaded(true);
+      //await SplashScreen.hideAsync();
+    }
+
+    prepare();
+  }, []);
+
+  if (!isLoaded) {
+    return null;
+  }
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
@@ -32,4 +64,4 @@ export default function RootLayout() {
       <StatusBar style="auto" />
     </ThemeProvider>
   );
-}
+});
