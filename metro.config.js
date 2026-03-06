23a3a39cdf571e17e1ac9b76c8e24bb576c6afd9
Natsune Mochizuki <me@natsuneko.cat>
2026-03-06 19:18:37 +0900

    chore: uniwind にする

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,10 @@
-const { withNativeWind } = require("nativewind/metro");
-const {
-  getSentryExpoConfig
-} = require("@sentry/react-native/metro");
+const { withUniwindConfig } = require("uniwind/metro");
+const { getSentryExpoConfig } = require("@sentry/react-native/metro");
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getSentryExpoConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: "./globals.css" });
+module.exports = withUniwindConfig(config, {
+  cssEntryFile: "./global.css",
+  dtsFile: "uniwind-types.d.ts",
+});
