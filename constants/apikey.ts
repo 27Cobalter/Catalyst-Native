36311e77a51aee57013342379e0c70560b9ba36b
Natsune Mochizuki <me@natsuneko.cat>
2026-03-09 21:41:19 +0900

    謎

--- constants/apikey.ts
+++ constants/apikey.ts
@@ -9,7 +9,7 @@ export const API_KEY = Platform.select({
   android: {
     clientId: process.env.EXPO_PUBLIC_CATALYST_FOR_ANDROID_CLIENT_ID,
     clientSecret: process.env.EXPO_PUBLIC_CATALYST_FOR_ANDROID_CLIENT_SECRET,
-    redirectUri: "com.natsuneko.catalyst://authorize",
+    redirectUri: process.env.NODE_ENV === "development" ? "exp+catalyst-native://authorize" : "com.natsuneko.catalyst://authorize",
   },
   web: {
     clientId: "",
