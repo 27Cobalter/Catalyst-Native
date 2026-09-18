caabeda4a182dbc858123429b15623dce5d35675
MOCHIZUKI Kanon <me@natsuneko.cat>
2026-09-18 23:10:50 +0900

    Apply batched suggestions from code review
    Co-authored-by: MOCHIZUKI Kanon <mikazuki_fuyuno@outlook.com>

--- apps/mobile/src/components/profile/timeline.tsx
+++ apps/mobile/src/components/profile/timeline.tsx
@@ -47,12 +47,12 @@ const ThumbnailCell = memo(({ status, cellSize }: { status: CatalystStatus; cell
               onLoadEnd={() => setIsImageLoading(false)}
             />
             {status.medias.some((w) => w.metadata?.isSensitive) && (
-              <View className="absolute inset-0 flex items-center justify-center bg-light-skeleton/50 dark:bg-dark-skeleton/50">
+              <View className="absolute inset-0 flex items-center justify-center">
                 <Text className="text-light-text dark:text-dark-text">Sensitive Content</Text>
               </View>
             )}
             {status.medias.some((w) => w.metadata?.isSpoiler) && (
-              <View className="absolute inset-0 flex items-center justify-center bg-light-skeleton/50 dark:bg-dark-skeleton/50">
+              <View className="absolute inset-0 flex items-center justify-center">
                 <Text className="text-light-text dark:text-dark-text">Spoiler Content</Text>
               </View>
             )}
